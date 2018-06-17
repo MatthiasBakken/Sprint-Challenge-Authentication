99abd364508a5a04d050fdc28b297b60b3c43802
Matthias Bakken <matthias.bakken@gmail.com>
2018-06-17 14:43:06 -0700

    finished user schema

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -42,6 +42,12 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
+  return bcrypt.compare(plainTextPW, this.password, function (err, match) {
+    if (err) {
+      return callBack(err.message);
+    }
+    callBack(null, match)
+  })
 };
 
 module.exports = mongoose.model('User', UserSchema);
